6edb3a2a39b1f14ff42bca8cea28c91250b66b20
RoderCode <Rodersnow@gmail.com>
2022-12-07 17:49:28 +0100

    add AddPictureToScene class

---
+++ src/main/java/Temples/AddPictureToScene.java
@@ -0,0 +1,15 @@
+package Temples;
+
+public class AddPictureToScene {
+//    @FXML
+//    ImageView imageView;
+//    Button myButton;
+//    Image image = new Image(new FileInputStream("src/main/resources/com/example/Game/images.png"));
+//
+//    public Control() throws FileNotFoundException {
+//    }
+//
+//    public void displayImage(){
+//        imageView.setImage(image);
+//    }
+}
