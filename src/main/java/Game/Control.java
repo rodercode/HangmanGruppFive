9920bc2033f71a4380b20350c576108f17b7b34e
RoderCode <Rodersnow@gmail.com>
2022-12-08 11:40:00 +0100

    add Window

--- src/main/java/Game/Control.java
+++ src/main/java/Game/Control.java
@@ -3,7 +3,7 @@ package Game;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class Control {
     @FXML
     private Label welcomeText;
 
--- src/main/java/Temples/AddImage.java
+++ src/main/java/Temples/AddImage.java
@@ -1,5 +1,13 @@
 package Temples;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class AddImage {
 //    @FXML
 //    ImageView imageView;
