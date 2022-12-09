72af7e4464503d950c189238f422533050e79b6b
nilsson46 <nilssonsimon46@gmail.com>
2022-12-09 09:44:27 +0100

    Made a text field that working with a button

--- src/main/java/Game/MouseInput.java
+++ src/main/java/Game/MouseInput.java
@@ -1,13 +1,24 @@
 package Game;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 public class MouseInput {
     @FXML
     private Label testText;
+    @FXML
+    private TextField guess;
+    @FXML
+    private Text theWordField;
 
     public void buttonQ() {
-        testText.setText("Q");
+        guess.setText("Q");
+    }
+
+    public void getUserInput(ActionEvent actionEvent) {
     }
 }
+
