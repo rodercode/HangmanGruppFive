efa0b577eee4fa5fe0d9f199ca87cb412028e4ba
RoderCode <Rodersnow@gmail.com>
2022-12-09 09:58:10 +0100

    add button, textField and Label to userInput.fxml

--- src/main/java/Game/Word.java
+++ src/main/java/Game/Word.java
@@ -1,5 +1,25 @@
 package Game;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Word {
 
+     @FXML
+     private TextField userInput;
+
+     @FXML
+     private Label wordToGuess;
+
+
+
+    public void btn() {
+        String word = userInput.getText();
+        wordToGuess.setText(word);
+    }
+
+
 }
+
