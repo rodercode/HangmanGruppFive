2df029a07fe9fad9afcfb7b06a83e3a9bc1eec61
RoderCode <Rodersnow@gmail.com>
2022-12-12 11:30:13 +0100

    add Constructor and Instance field List

--- src/main/java/Game/Database.java
+++ src/main/java/Game/Database.java
@@ -1,15 +1,26 @@
 package Game;
 
+import javafx.scene.LightBase;
 import javafx.scene.chart.PieChart;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Database {
 
     private static final Database instance = new Database();
 
+    private List<String> listOfWords;
+
+    public Database() {
+        listOfWords = new ArrayList<>();
+
+    }
+
     public static Database getInstance(){
         return instance;
     }
-    
+
 
 
 
