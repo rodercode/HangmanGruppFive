b764adeea8450d24b503bd0f43cc3f11f5fd3640
RoderCode <Rodersnow@gmail.com>
2022-12-12 11:28:42 +0100

    add Singleton design pattern

--- src/main/java/Game/Database.java
+++ src/main/java/Game/Database.java
@@ -1,9 +1,17 @@
 package Game;
 
+import javafx.scene.chart.PieChart;
+
 public class Database {
 
     private static final Database instance = new Database();
 
+    public static Database getInstance(){
+        return instance;
+    }
     
+
+
+
 
 }
