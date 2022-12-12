956c30d1ea43f952b2a98fd269d69a2726699684
RoderCode <Rodersnow@gmail.com>
2022-12-12 15:52:50 +0100

    Add List of players

--- src/main/java/Game/Player.java
+++ src/main/java/Game/Player.java
@@ -1,5 +1,11 @@
 package Game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
-    
+    private List<String>players;
+    public Player() {
+        players = new ArrayList<>();
+    }
 }
