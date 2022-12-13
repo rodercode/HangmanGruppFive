31523bb038c68957df3d2bf7d627e8774ed65126
RoderCode <Rodersnow@gmail.com>
2022-12-13 16:48:35 +0100

    clean GameViewControl class and change name on enter button and sett userinput to empty after every attempt

--- src/main/java/Game/GameViewControl.java
+++ src/main/java/Game/GameViewControl.java
@@ -24,25 +24,234 @@ import java.util.ResourceBundle;
 
 public class GameViewControl implements Initializable {
 
-    @FXML
-    public void button() {
-        String word = userInput.getText();
-        String hiddenWord = " ";
-        for (int i = 0; i <word.length() ; i++) {
-            hiddenWord = hiddenWord + " _ ";
-        }
-        wordGuess.setText(hiddenWord);
-    }
-    @FXML
-    ImageView imageViewCake12;
-    Image imageCake12 = new Image(new FileInputStream("src/main/resources/IMG/Cake12.png"));
-    @FXML
-    Button buttonToSwitchImages;
-
+    // Variables
     private Database data;
     private int remainingGuesses;
 
+    // FXML variables
+    @FXML
+    private Label currentPlayer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Label wordGuess;
+    @FXML
+    private Label mistake;
+    @FXML
+    private ImageView imageViewCake;
 
+    public GameViewControl() throws FileNotFoundException {
+        data = Database.getInstance();
+        remainingGuesses = 11;
+    }
+
+    // Start method
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        imageViewCake.setImage(imageCake12);
+        wordGuess.setAlignment(Pos.CENTER);
+    }
+
+
+
+
+
+    @FXML
+    public void pressEnter() {
+        String word = data.getListOfWords().get(2);
+        if (userInput.getText().toUpperCase().equals(word)) {
+            System.out.println("Funkade :D");
+        } else {
+       /*int count = 0;
+         count++;
+         mistake.setText(count); */
+            System.out.println("Fel");
+        }
+        userInput.setText("");
+    }
+
+    // Cake animation
+    @FXML
+    public void displayCakeImage() {
+        if (remainingGuesses == 11)
+            imageViewCake.setImage(imageCake11);
+        else if (remainingGuesses == 10)
+            imageViewCake.setImage(imageCake10);
+        else if (remainingGuesses == 9)
+            imageViewCake.setImage(imageCake9);
+        else if (remainingGuesses == 8)
+            imageViewCake.setImage(imageCake8);
+        else if (remainingGuesses == 7)
+            imageViewCake.setImage(imageCake7);
+        else if (remainingGuesses == 6)
+            imageViewCake.setImage(imageCake6);
+        else if (remainingGuesses == 5)
+            imageViewCake.setImage(imageCake5);
+        else if (remainingGuesses == 4)
+            imageViewCake.setImage(imageCake4);
+        else if (remainingGuesses == 3)
+            imageViewCake.setImage(imageCake3);
+        else if (remainingGuesses == 2)
+            imageViewCake.setImage(imageCake2);
+        else if (remainingGuesses == 1)
+            imageViewCake.setImage(imageCake1);
+        else if (remainingGuesses == 0)
+            imageViewCake.setImage(imageCake0);
+        remainingGuesses--;
+    }
+    // End of Cake Animation
+
+
+    // Letter buttons
+    @FXML
+    public void buttonQ() {
+        userInput.appendText("Q");
+    }
+
+    @FXML
+    public void buttonW() {
+        userInput.appendText("W");
+    }
+
+    @FXML
+    public void buttonE() {
+        userInput.appendText("E");
+    }
+
+    @FXML
+    public void buttonR() {
+        userInput.appendText("R");
+    }
+
+    @FXML
+    public void buttonT() {
+        userInput.appendText("T");
+    }
+
+    @FXML
+    public void buttonY() {
+        userInput.appendText("Y");
+    }
+
+    @FXML
+    public void buttonU() {
+        userInput.appendText("U");
+    }
+
+    @FXML
+    public void buttonI() {
+        userInput.appendText("I");
+    }
+
+    @FXML
+    public void buttonO() {
+        userInput.appendText("O");
+    }
+
+    @FXML
+    public void buttonP() {
+        userInput.appendText("P");
+    }
+
+    @FXML
+    public void buttonÅ() {
+        userInput.appendText("Å");
+    }
+
+    @FXML
+    public void buttonA() {
+        userInput.appendText("A");
+    }
+
+    @FXML
+    public void buttonS() {
+        userInput.appendText("S");
+    }
+
+    @FXML
+    public void buttonD() {
+        userInput.appendText("D");
+    }
+
+    @FXML
+    public void buttonF() {
+        userInput.appendText("F");
+    }
+
+    @FXML
+    public void buttonG() {
+        userInput.appendText("G");
+    }
+
+    @FXML
+    public void buttonH() {
+        userInput.appendText("H");
+    }
+
+    @FXML
+    public void buttonJ() {
+        userInput.appendText("J");
+    }
+
+    @FXML
+    public void buttonK() {
+        userInput.appendText("K");
+    }
+
+    @FXML
+    public void buttonL() {
+        userInput.appendText("L");
+    }
+
+    @FXML
+    public void buttonÖ() {
+        userInput.appendText("Ö");
+    }
+
+    @FXML
+    public void buttonÄ() {
+        userInput.appendText("Ä");
+    }
+
+    @FXML
+    public void buttonZ() {
+        userInput.appendText("Z");
+    }
+
+    @FXML
+    public void buttonX() {
+        userInput.appendText("X");
+    }
+
+    @FXML
+    public void buttonC() {
+        userInput.appendText("C");
+    }
+
+    @FXML
+    public void buttonV() {
+        userInput.appendText("V");
+    }
+
+    @FXML
+    public void buttonB() {
+        userInput.appendText("B");
+    }
+
+    @FXML
+    public void buttonN() {
+        userInput.appendText("N");
+    }
+
+    @FXML
+    public void buttonM() {
+        userInput.appendText("M");
+    }
+
+    // end of Letter buttons
+
+    // Images
+    Image imageCake12 = new Image(new FileInputStream("src/main/resources/IMG/Cake12.png"));
     Image imageCake11 = new Image(new FileInputStream("src/main/resources/IMG/Cake11.png"));
 
     Image imageCake10 = new Image(new FileInputStream("src/main/resources/IMG/Cake10.png"));
@@ -67,189 +276,6 @@ public class GameViewControl implements Initializable {
 
     Image imageCake0 = new Image(new FileInputStream("src/main/resources/IMG/Cake0.png"));
 
-   @Override
-   public void initialize(URL url, ResourceBundle resourceBundle) {
-      currentPlayer.setAlignment(Pos.CENTER);
-      wordGuess.setAlignment(Pos.CENTER);
-      mistake.setAlignment(Pos.CENTER);
-      data.getListOfWords();
-      imageViewCake.setImage(imageCake12);
-
-   }
-   public GameViewControl() throws FileNotFoundException {
-      data = Database.getInstance();
-       remainingGuesses = 11;
-   }
-
-   @FXML
-   private Label currentPlayer;
-   @FXML
-   private TextField userInput;
-   @FXML
-   private Label wordGuess;
-   @FXML
-   private Label mistake;
-
-  @FXML
-   private ImageView imageViewCake;
-
-
-    @FXML
-    public void displayCakeImage() {
-        if(remainingGuesses == 11)
-            imageViewCake.setImage(imageCake11);
-        else if(remainingGuesses == 10)
-            imageViewCake.setImage(imageCake10);
-        else if(remainingGuesses == 9)
-            imageViewCake.setImage(imageCake9);
-        else if(remainingGuesses == 8)
-            imageViewCake.setImage(imageCake8);
-        else if(remainingGuesses == 7)
-            imageViewCake.setImage(imageCake7);
-        else if(remainingGuesses == 6)
-            imageViewCake.setImage(imageCake6);
-        else if(remainingGuesses == 5)
-            imageViewCake.setImage(imageCake5);
-        else if(remainingGuesses == 4)
-            imageViewCake.setImage(imageCake4);
-        else if(remainingGuesses == 3)
-            imageViewCake.setImage(imageCake3);
-        else if(remainingGuesses == 2)
-            imageViewCake.setImage(imageCake2);
-        else if(remainingGuesses == 1)
-            imageViewCake.setImage(imageCake1);
-        else if(remainingGuesses == 0)
-            imageViewCake.setImage(imageCake0);
-        remainingGuesses--;
-    }
-
-   public void userWordGuess () {
-      String word = data.getListOfWords().get(2);
-      if (userInput.getText().equals(word)) {
-         System.out.println("Funkade :D");
-      } else {
-       /*int count = 0;
-         count++;
-         mistake.setText(count); */
-         System.out.println("Fel");
-      }
-   }
-
-   @FXML
-   public void buttonQ() {
-      userInput.appendText("Q");
-   }
-   @FXML
-   public void buttonW() {
-      userInput.appendText("W");
-   }
-   @FXML
-   public void buttonE() {
-      userInput.appendText("E");
-   }
-   @FXML
-   public void buttonR() {
-      userInput.appendText("R");
-   }
-   @FXML
-   public void buttonT() {
-      userInput.appendText("T");
-   }
-   @FXML
-   public void buttonY() {
-      userInput.appendText("Y");
-   }
-   @FXML
-   public void buttonU() {
-      userInput.appendText("U");
-   }
-   @FXML
-   public void buttonI() {
-      userInput.appendText("I");
-   }
-   @FXML
-   public void buttonO() {
-      userInput.appendText("O");
-   }
-   @FXML
-   public void buttonP() {
-      userInput.appendText("P");
-   }
-   @FXML
-   public void buttonÅ() {
-      userInput.appendText("Å");
-   }
-   @FXML
-   public void buttonA() {
-      userInput.appendText("A");
-   }
-   @FXML
-   public void buttonS() {
-      userInput.appendText("S");
-   }
-   @FXML
-   public void buttonD() {
-      userInput.appendText("D");
-   }
-   @FXML
-   public void buttonF() {
-      userInput.appendText("F");
-   }
-   @FXML
-   public void buttonG() {
-      userInput.appendText("G");
-   }
-   @FXML
-   public void buttonH() {
-      userInput.appendText("H");
-   }
-   @FXML
-   public void buttonJ() {
-      userInput.appendText("J");
-   }
-   @FXML
-   public void buttonK() {
-      userInput.appendText("K");
-   }
-   @FXML
-   public void buttonL() {
-      userInput.appendText("L");
-   }
-   @FXML
-   public void buttonÖ() {
-      userInput.appendText("Ö");
-   }
-   @FXML
-   public void buttonÄ() {
-      userInput.appendText("Ä");
-   }
-   @FXML
-   public void buttonZ() {
-      userInput.appendText("Z");
-   }
-   @FXML
-   public void buttonX() {
-      userInput.appendText("X");
-   }
-   @FXML
-   public void buttonC() {
-      userInput.appendText("C");
-   }
-   @FXML
-   public void buttonV() {
-      userInput.appendText("V");
-   }
-   @FXML
-   public void buttonB() {
-      userInput.appendText("B");
-   }
-   @FXML
-   public void buttonN() {
-      userInput.appendText("N");
-   }
-   @FXML
-   public void buttonM() {
-      userInput.appendText("M");
-   }
+    // end of Images
 }
 
