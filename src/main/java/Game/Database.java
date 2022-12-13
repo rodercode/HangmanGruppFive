ffdec7bbe7406b29b2ec6287355edb0b190efbf8
RoderCode <Rodersnow@gmail.com>
2022-12-13 21:13:09 +0100

    add playerScores hashmap, instance field score and addScore

--- src/main/java/Game/Database.java
+++ src/main/java/Game/Database.java
@@ -10,15 +10,22 @@ public class Database {
     private static final Database instance = new Database();
 
    private HashMap<Integer, String> listOfWords;
-
+   private HashMap<Integer,Integer> playerScores;
    private List<Integer> players;
-
+   private int score;
 
     public Database() {
         listOfWords = new HashMap<>();
+        playerScores = new HashMap<>();
         players = Arrays.asList(1,2);
         listOfWords.put(1,"Kaffe");
         listOfWords.put(2,"TE");
+        score = 0;
+    }
+
+    public void addScore(int player){
+        score = score +5;
+        playerScores.put(player,score);
     }
 
     public void addWord(int player, String word){
